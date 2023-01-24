8544d34ac4c6c4e2e0a41936f967f4da389a297b
Elmer Jose Muñoz Zuñiga <67802793+Ejmz216@users.noreply.github.com>
2023-01-23 20:09:42 -0500

    V1.5_ Style fix

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,12 +4,10 @@ import { NavBar } from "../components/NavBar/NavBar.jsx";
 import { Banner } from "../components/Banner/Banner.jsx";
 import { Skills } from "../components/Skills/Skills.jsx";
 import { Cards } from "../components/Projects/Cards.jsx";
-/* import { Projects } from "../components/Projects";
-import { Contact } from "../components/Contact"; */
 import { Footer } from "../components/Footer/Footer.jsx";
 import { ScrollUp } from "../components/ScrollUp/ScrollUp.jsx";
-/* import { Contact } from "../components/Contact/Contact.jsx";
- */
+/* import { Contact } from "../components/Contact/Contact.jsx"; */
+
 function App() {
   return (
     <div className="App">
